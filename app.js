a44b345a05339fc1bd006416223b39ffa33c2f67
Victor Oliveira <victorlaertedoliveira@gmail.com>
2016-03-23 02:21:31 -0300

    Revert "Adicionando serviço rest de listagem e angular populando tela com angular e jade"
    This reverts commit 6ce8d84058b15bf545d7fa6593d73fe44cbdb1ac.

--- app.js
+++ app.js
@@ -4,13 +4,9 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-//Database
-var mongo = require('mongodb');
-var monk = require('monk');
-var db = monk('localhost:27017/eventcounter');
 
 var routes = require('./routes/index');
-var events = require('./routes/events');
+var users = require('./routes/users');
 
 var app = express();
 
@@ -26,14 +22,8 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(function(req, res, next){
-  //Adding db to each request, review this code.
-  req.db = db;
-  next();
-});
-
 app.use('/', routes);
-app.use('/events', events);
+app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
