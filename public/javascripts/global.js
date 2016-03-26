ed239beef3b412ab7e75e0c82c229525472b19ec
Victor Oliveira <victorlaertedoliveira@gmail.com>
2016-03-25 22:50:25 -0300

    Adicionando evento (ainda sem validação)

--- public/javascripts/global.js
+++ public/javascripts/global.js
@@ -1,41 +1,59 @@
 var app = angular.module('eventCounter', []);
 
-app.controller('eventCounterCtrl', function($scope,  $http) {
+app.controller('eventCounterCtrl', function($scope, $http) {
 
-	$scope.selectedEvent = undefined;
-    $scope.todayEventList = [];
-    $scope.pastEventList = [];
-    $scope.nextEventList = [];
+    $scope.init = function() {
 
-    $http.get('/events/eventlist').then(function(response){
+        $scope.selectedEvent = undefined;
+        $scope.todayEventList = [];
+        $scope.pastEventList = [];
+        $scope.nextEventList = [];
+        $scope.eventToAdd = {};
+        $scope.showForm = false;
 
-    	//PASS TODAY PARAMETERS
-    	if (response.status == 200){
-    		$scope.todayEventList = response.data;
-    	}
+        $http.get('/events/eventlist').then(function(response) {
 
-    });
+            //PASS TODAY PARAMETERS
+            if (response.status == 200) {
+                $scope.todayEventList = response.data;
+                console.log(JSON.stringify($scope.todayEventList));
+            }
 
-    $http.get('/events/eventlist').then(function(response){
+        });
 
-    	//PASS FUTURE PARAMETERS
-    	if (response.status == 200){
-    		$scope.nextEventList = response.data;
-    	}
-    });
+        $http.get('/events/eventlist').then(function(response) {
 
-    $http.get('/events/eventlist').then(function(response){
+            //PASS FUTURE PARAMETERS
+            if (response.status == 200) {
+                $scope.nextEventList = response.data;
+            }
+        });
 
-    	//PASS PAST PARMETERS
-    	if (response.status == 200){
-    		$scope.pastEventList = response.data;
-    	}
-    });
+        $http.get('/events/eventlist').then(function(response) {
+
+            //PASS PAST PARMETERS
+            if (response.status == 200) {
+                $scope.pastEventList = response.data;
+            }
+        });
+    }
 
     $scope.showEventInfo = function(event) {
 
-    	$scope.selectedEvent = event;
-
-    	console.log($scope.selectedEvent);
+        $scope.selectedEvent = event;
     }
+
+    $scope.sendEventRequest = function() {
+
+        //DO ALL VALIDATIONS
+
+        $http.post('/events/addevent', $scope.eventToAdd).then(function(response) {
+            console.log(response);
+            $scope.init();
+        }, function(response) {
+            console.log(response);
+        });
+    }
+
+     $scope.init();
 });
--- routes/events.js
+++ routes/events.js
@@ -2,17 +2,45 @@ var express = require('express');
 var router = express.Router();
 
 /*
-* GET eventList
-*/
-router.get('/eventlist', function(req, res){
+ * GET eventList
+ */
+router.get('/eventlist', function(req, res) {
 
 	var db = req.db;
-	console.log(db);
 	var collection = db.get('eventlist');
 
-	collection.find({},{}, function(e, docs){
+	//TODO: if admin return all, if not admin return only checked true
+	collection.find({}, {}, function(e, docs) {
 		res.json(docs);
 	});
 });
+/*
+ * POST to add event
+ */
+
+router.post('/addevent', function(req, res) {
+
+	var db = req.db;
+	var collection = db.get('eventlist');
+
+	var eventToAdd = req.body;
+	eventToAdd.finishDate = eventToAdd.startDate;
+	eventToAdd.checked = false;
+	eventToAdd.counter = 0;
+
+	//DO ALL VALIDATIONS
+
+	collection.insert(eventToAdd, function(err, result) {
+		res.send(
+			(err === null) ? {
+				msg: ''
+			} : {
+				msg: err
+			}
+		);
+	});
+
+});
+
 
 module.exports = router;
